bf66c4e48600364efd8cde1245e7e2122f7a489a
NunoNartMatias <nunosilvamatias@gmail.com>
2023-04-21 18:04:55 +0100

    Added GetName and TakeDamage methods to Foe Class

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -16,5 +16,33 @@ namespace GameSix
         health = 100;
         shield = 0;
        }
+    
+        // Method that get the name of the Foe and returns it
+        public string GetName()
+        {
+            return name;
+        }
+
+        // Method that calculates damage taken by Foe
+        public void TakeDamage(float damage)
+        {
+            
+            shield -= damage;
+            // damage was enough to destroy the shield aka shield is lower than 0
+            if (shield < 0)
+            {
+                // damage that still needs to be done = - shield
+                // -shield represents how much damage the shield was not able to block
+                float damageStillToInflict = -shield;
+                // need to ask why shield = 0 
+                shield = 0;
+                /* health of enemy will be reduced by the damage
+                that still needs to be inflicted*/
+                health -= damageStillToInflict;
+                /* we don´t want negative health so we make sure to put it at 0
+                in case the damage number was higher than the health one*/
+                if ( health < 0) health = 0;
+            }
+        }
     }
 }
