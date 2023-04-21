e5b9efa3112e141397794330cdc7d9733e679f49
NunoNartMatias <nunosilvamatias@gmail.com>
2023-04-21 17:49:19 +0100

    Added Constructor to Foe Class

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -4,6 +4,17 @@ namespace GameSix
 {
     public class Foe
     {
-        
+        // variables
+        private string name;
+        private float health;
+        private float shield;
+
+        // Foe Constructor
+       public Foe(string name) 
+       {
+        this.name = name;
+        health = 100;
+        shield = 0;
+       }
     }
 }
